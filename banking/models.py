bad806844b22276406ab4ed8e9e5bfdac56d3357
mmmurka <murenkodan2005@gmail.com>
2024-02-26 18:49:16 +0200

    maked migration

--- banking/models.py
+++ banking/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=20)
+    balance = models.IntegerField(default=0)
+    identification_number = models.PositiveIntegerField(
+        default=10,
+        unique=True,
+        validators=[MinValueValidator(1000000)],
+        db_comment='Personal identification number with 8 digits'
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}'s Account"
--- online_bank/settings.py
+++ online_bank/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'online_bank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bank',
+        'USER': 'mmmurka',
+        'PASSWORD': '12341',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
@@ -118,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
