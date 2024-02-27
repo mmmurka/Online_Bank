2695c6b0824905b557ab4135f738dbeedce1b981
mmmurka <murenkodan2005@gmail.com>
2024-02-27 18:11:31 +0200

    unstable version

--- banking/urls.py
+++ banking/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
--- banking/views.py
+++ banking/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
-
-
 def home(request):
     return render(request, 'banking/home.html')
+
+
+def login(request):
+    return render(request, 'banking/login.html')
+
+
+def signup(request):
+
+    return render(request, 'banking/signup.html')
+
+
