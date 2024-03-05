27d9d720716072470e4825a0e9b1770c45ee9e49
mmmurka <murenkodan2005@gmail.com>
2024-03-05 23:28:33 +0200

    added Transaction model

--- banking/models.py
+++ banking/models.py
@@ -6,3 +6,10 @@ from django.core.validators import MinValueValidator
 class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     balance = models.IntegerField(default=0)
+
+
+class Transaction(models.Model):
+    sender = models.ForeignKey(Account, related_name='sent_transactions', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(Account, related_name='received_transactions', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
