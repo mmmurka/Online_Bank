961dd695e5083b96815eef8d43f2fce449f54315
mmmurka <murenkodan2005@gmail.com>
2024-03-05 23:42:29 +0200

    updated models

--- banking/models.py
+++ banking/models.py
@@ -1,11 +1,24 @@
-from django.db import models
+from django.db import models, transaction
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
 
 
 class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    balance = models.IntegerField(default=0)
+    balance = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+
+    def deposit(self, amount):
+        with transaction.atomic():
+            self.balance += amount
+            self.save()
+
+    def withdraw(self, amount):
+        with transaction.atomic():
+            if self.balance >= amount:
+                self.balance -= amount
+                self.save()
+                return True
+        return False
 
 
 class Transaction(models.Model):
@@ -13,3 +26,11 @@ class Transaction(models.Model):
     receiver = models.ForeignKey(Account, related_name='received_transactions', on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    @classmethod
+    def create_transaction(cls, sender, receiver, amount):
+        with transaction.atomic():
+            if sender.withdraw(amount):
+                receiver.deposit(amount)
+                return cls.objects.create(sender=sender, receiver=receiver, amount=amount)
+        return None
