bede2a73164b870d80e3d60ad78df2c6e389bc8e
mmmurka <murenkodan2005@gmail.com>
2024-03-11 23:36:05 +0200

    fixed bugs exchange_rate(if status code != 200, rate will return rate 0)

--- banking/exchange_rate.py
+++ banking/exchange_rate.py
@@ -1,24 +1,40 @@
 import requests
+import logging
 from django.core.cache import cache
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def get_exchange_rate():
-    # Пытаемся получить данные из кеша
-    cached_data = cache.get('exchange_rate')
+    try:
+        # Пытаемся получить данные из кеша
+        cached_data = cache.get('exchange_rate')
 
-    if cached_data:
-        return cached_data
+        if cached_data:
+            return cached_data
 
-    url = 'https://api.monobank.ua/bank/currency'
-    response = requests.get(url)
+        else:
+            url = 'https://api.monobank.ua/bank/currency'
+            response = requests.get(url)
 
-    if response.status_code == 200:
-        result = response.json()
-        uah_to_usd_rate = result[0]['rateSell']
-        uah_to_eur_rate = result[1]['rateSell']
+            if response.status_code == 200:
+                result = response.json()
+                uah_to_usd_rate = result[0]['rateSell']
+                uah_to_eur_rate = result[1]['rateSell']
 
-        # Сохраняем данные в кеше
-        cache.set('exchange_rate', (uah_to_usd_rate, uah_to_eur_rate), 5 * 60)  # 5 минут
-        return uah_to_usd_rate, uah_to_eur_rate
-    else:
-        print(f"Failed to fetch data. Status code: {response.status_code}")
+                # Сохраняем данные в кеше
+                cache.set('exchange_rate', (uah_to_usd_rate, uah_to_eur_rate), 5 * 60)  # 5 минут
+                logger.info(f"Data saved in cache: USD {uah_to_usd_rate}, EUR {uah_to_eur_rate}")
+                return uah_to_usd_rate, uah_to_eur_rate
+            else:
+                logger.error(f"Failed to fetch data. Status code: {response.status_code}")
+                uah_to_usd_rate = 0.00
+                uah_to_eur_rate = 0.00
+                return uah_to_usd_rate, uah_to_eur_rate
+
+    except Exception as e:
+        logger.error(f"Failed to fetch data. Error: {e}")
+
+
