36f7d6d09081bb600d807858ff5e75bb54bd5a5b
mmmurka <murenkodan2005@gmail.com>
2024-03-13 19:57:40 +0200

    unstable(don`t work password reset)

--- banking/urls.py
+++ banking/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -10,4 +11,11 @@ urlpatterns = [
     path('logout/', views.user_logout, name='logout'),
     path('transfer/', views.transfer, name='transfer'),
     path('success_transfer/', views.success_transfer, name='success_transfer'),
+    # Стандартные представления Django для сброса пароля
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
+
+
