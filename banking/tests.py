8b1355a9f865e69b8ae3a0f58288853005d796a9
mmmurka <murenkodan2005@gmail.com>
2024-03-21 22:44:12 +0200

    wrote test home page

--- banking/tests.py
+++ banking/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Account
 
-# Create your tests here.
+
+class ViewsTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', email='test@example.com', password='testpassword')
+        self.account = Account.objects.create(user=self.user)
+
+    def test_home_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'banking/home.html')
+
